736a73414fb3e0e234299a1ff25931a5d50487cc
unknown <nehathakur764@gmail.com>
2020-11-25 01:01:41 +0530

    Validate EmailId With Three Mandatory Parts

--- src/UserRegistration/UserRegistration.java
+++ src/UserRegistration/UserRegistration.java
@@ -1,11 +1,10 @@
 package UserRegistration;
 import java.util.Scanner;
 public class UserRegistration {
-    Scanner sc;
+    Scanner sc=new Scanner(System.in);
 
     /* UC1: Validate First Name  */
-    private void firstName() {
-        sc=new Scanner(System.in);
+    private void ValidfirstName() {
         System.out.println("Enter The First Name");
         String firstname=sc.nextLine();
         if (firstname.matches("^[A-Z]{1}[a-z]{2,}"))
@@ -15,8 +14,7 @@ public class UserRegistration {
     }
 
     /* UC2: Validate Last Name  */
-    private void lastName(){
-        sc=new Scanner(System.in);
+    private void ValidlastName() {
         System.out.println("Enter The Last Name");
         String lastname=sc.nextLine();
         if (lastname.matches("[A-Z]{1}[a-z]{2,}"))
@@ -25,19 +23,32 @@ public class UserRegistration {
             System.out.println("Invalid LastName");
     }
 
+    /* UC1: Validate EmailId  */
+    private void validEmail() {
+        System.out.println("Enter The Email Id");
+        String emailid=sc.nextLine();
+        if (emailid.matches("^[A-Z0-9a-z.%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$"))
+            System.out.println("Valid EmailId");
+        else
+            System.out.println("Invalid EmailId");
+    }
+
     public static void main(String []args){
         UserRegistration userregistration=new UserRegistration();
         Scanner sc=new Scanner(System.in);
         while (true){
-            System.out.println("WELCOME TO USER REGISTRATION");
-            System.out.println("\nSelect Any Case To Validate" + "\n1. FirstName" + "\n2. LastName");
+            System.out.println("\n-------WELCOME TO USER REGISTRATION--------");
+            System.out.println("\nSelect Any Case To Validate" + "\n1. FirstName" + "\n2. LastName" + "\n3. Email");
             int ch=sc.nextInt();
             switch (ch) {
                 case 1:
-                    userregistration.firstName();
+                    userregistration.ValidfirstName();
                     break;
                 case 2:
-                    userregistration.lastName();
+                    userregistration.ValidlastName();
+                    break;
+                case 3:
+                    userregistration.validEmail();
                     break;
                 default:
                     System.out.println("Quit");
