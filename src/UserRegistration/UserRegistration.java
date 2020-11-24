e0c8ad6372a707be62e242fb2c288f5d7239879b
unknown <nehathakur764@gmail.com>
2020-11-24 23:48:44 +0530

    FistName start with cap,Minimum 3 character

--- src/UserRegistration/UserRegistration.java
+++ src/UserRegistration/UserRegistration.java
@@ -1,8 +1,21 @@
 package UserRegistration;
+import java.util.Scanner;
 public class UserRegistration {
+Scanner sc=new Scanner(System.in);
 
+    private void firstName() {
+        System.out.println("Enter the First Name");
+        String firstName=sc.nextLine();
+        if (firstName.matches("^[A-Z]{1}[a-z]{2,}"))
+            System.out.println("Valid FirstName");
+        else
+            System.out.println("Invalid FirstName");
+    }
+    
     public static void main(String []args){
         System.out.println("WELCOME TO USER REGISTRATION");
+        UserRegistration userregistration=new UserRegistration();
+        userregistration.firstName();
 
     }
 }
