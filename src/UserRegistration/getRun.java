5004f6034d86a604b5085228155b34aa385c4e03
unknown <nehathakur764@gmail.com>
2020-11-27 17:42:15 +0530

    New Changes

---
+++ src/UserRegistration/getRun.java
@@ -0,0 +1,9 @@
+package UserRegistration;
+
+public class getRun {
+    public static void main(String[] args){
+        Person person = new Person();
+        UserRegistration validate = new UserRegistration();
+        validate.validFirstName(person);
+    }
+}
